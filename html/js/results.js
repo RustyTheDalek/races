95df98b49b29d3392456563b00a729db067da150
Ian <enigma994@live.co.uk>
2024-08-03 12:33:32 +0100

    Added Race results to NUI

---
+++ html/js/results.js
@@ -0,0 +1,69 @@
+let resultsContainer = $('#results_container');
+let resultsTable = resultsContainer.find('table');
+let trackNameText = resultsContainer.find('.race_name');
+let lapsText = resultsContainer.find('.laps');
+let resultsTableData = resultsTable.find('tbody');
+
+let leaderboardDisplayLength = 5;
+
+$(function () {
+    window.addEventListener("message", readResultEvents);
+});
+
+function readResultEvents(event) {
+    let data = event.data;
+
+    if(data.type !== "results") return;
+
+    console.log("Race results");
+
+    switch(data.action) {
+        case "show_race_results":
+            console.log("showing race results");
+            showRaceResults(data.results, data.numberOfLaps, data.trackName);
+            break;
+    }
+}
+
+function showRaceResults(results, laps, trackName) {
+
+    resultsTableData.empty();
+
+    populateResults(results);
+
+    lapsText.text(laps > 1 ? `${laps} laps` : '' );
+    trackNameText.text(trackName)
+
+
+    resultsContainer.addClass('visible');
+
+    setTimeout(() => {
+        resultsContainer.removeClass('visible');
+    }, (leaderboardDisplayLength) * 1000);
+}
+
+function populateResults(results) {
+    results.forEach((result) => {
+        addResult(result);
+    });
+}
+
+function addResult(result) {
+
+    console.log(result);
+
+    let resultRow = $("<tr/>");
+
+    resultRow.append(createTD(result.position));
+    resultRow.append(createTD(result.playerName));
+    resultRow.append(createTD(result.time));
+    resultRow.append(createTD(result.fastestLap));
+    resultRow.append(createTD(result.vehicleName));
+    resultRow.append(createTD(result.averageFPS.toFixed(0)));
+
+    resultsTable.find('tbody').append(resultRow);
+}
+
+function createTD(text) {
+    return $("<td/>", { text: text });
+}
